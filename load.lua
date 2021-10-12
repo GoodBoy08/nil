41bdc223f17c2bb5c9a29be5d613124c3d1aa521
catboy08 <86081255+catboy08@users.noreply.github.com>
2021-10-12 12:37:16 -0700

    Create load.lua

---
+++ load.lua
@@ -0,0 +1,10 @@
+local v = loadstring(game:HttpGet("https://raw.githubusercontent.com/catboy08/nil/modules/function.lua"))()
+if v.v("3095204897") then
+  print("Isle")
+elseif v.v("4991214437") then
+  print("Town")
+elseif v.v("3260590327") then
+  print("TDS")
+elseif v.v("5041144419") then
+  print("SCP Roleplay")
+end
