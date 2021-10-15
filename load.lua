38498b1213e14ca8276bd41d43673ceb895e2d40
catboy08 <86081255+catboy08@users.noreply.github.com>
2021-10-15 11:15:12 -0700

    Update load.lua

--- load.lua
+++ load.lua
@@ -2,7 +2,7 @@ local v = loadstring(game:HttpGet("https://raw.githubusercontent.com/catboy08/ni
 if v.v("3095204897") then
   print("Isle")
 elseif v.v("4991214437") then
-  print("Town")
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/catboy08/nil/scripts/gamez/town.lua"))()
 elseif v.v("3260590327") then
   print("TDS")
 elseif v.v("5041144419") then
