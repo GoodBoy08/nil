d30319d9828a22051df982b5ed730ff9432bced8
catboy08 <86081255+catboy08@users.noreply.github.com>
2021-10-19 07:03:28 -0700

    Update load.lua

--- load.lua
+++ load.lua
@@ -1,10 +1,10 @@
-local v = loadstring(game:HttpGet("https://raw.githubusercontent.com/catboy08/nil/modules/function.lua"))()
-if v.v("3095204897") then
+
+if game.PlaceId == ("3095204897") then
   print("Isle")
-elseif v.v("4991214437") then
+elseif game.PlaceId == ("4991214437") then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/catboy08/nil/scripts/gamez/town.lua"))()
-elseif v.v("3260590327") then
+elseif game.PlaceId == ("3260590327") then
   print("TDS")
-elseif v.v("5041144419") then
+elseif game.PlaceId == ("5041144419") then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/catboy08/nil/scripts/gamez/scp-roleplay.lua"))()
 end
