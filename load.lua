61d5c33f3110afd61beebf629cd190bcdeea29b5
coolb0y08 <93867745+coolb0y08@users.noreply.github.com>
2021-11-15 22:46:00 +0200

    Update load.lua

--- load.lua
+++ load.lua
@@ -1,9 +1,9 @@
 local library = {}
 library.loadmodule = function(t)
-    return loadstring(game:HttpGet("https://raw.githubusercontent.com/catboy08/nil/main/modules/"..t))()
+    return loadstring(game:HttpGet("https://raw.githubusercontent.com/catboy08/nil/main/modules/"..t..".lua"))()
 end
 
 library.load = function(t)
-    return loadstring(game:HttpGet("https://raw.githubusercontent.com/catboy08/nil/main/games/"..t.PlaceId))()
+    return loadstring(game:HttpGet("https://raw.githubusercontent.com/catboy08/nil/main/games/"..t.PlaceId..".lua"))()
 end
 return library
