ecc6e3446dbb7023ba723a0970d6203565b29d2c
coolb0y08 <93867745+coolb0y08@users.noreply.github.com>
2021-11-15 21:49:01 +0200

    Update load.lua

--- load.lua
+++ load.lua
@@ -3,4 +3,7 @@ library.loadmodule = function(t)
     return loadstring(game:HttpGet("https://raw.githubusercontent.com/catboy08/nil/main/modules/"..t))()
 end
 
+library.load = function(t)
+    return loadstring(game:HttpGet("https://raw.githubusercontent.com/catboy08/nil/main/games/"..t))()
+end
 return library
