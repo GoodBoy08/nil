d660082e71ea3cc7aece426c899880f5c477c9b3
coolb0y08 <93867745+coolb0y08@users.noreply.github.com>
2021-11-19 20:27:56 +0200

    Delete load.lua

--- load.lua
+++
@@ -1,9 +0,0 @@
-local library = {}
-library.loadmodule = function(t)
-    return loadstring(game:HttpGet("https://raw.githubusercontent.com/catboy08/nil/main/modules/"..t..".lua"))()
-end
-
-library.load = function(t)
-    return loadstring(game:HttpGet("https://raw.githubusercontent.com/catboy08/nil/main/games/"..t.PlaceId..".lua"))()
-end
-return library
